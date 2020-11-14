100b347355f21692e886e0611e62f20270213f71
Clucas0311 <clucas0311@gmail.com>
2020-11-13 21:31:29 -0500

    Adding JSX comments

--- src/index.js
+++ src/index.js
@@ -4,7 +4,24 @@ import ReactDOM from 'react-dom';
 
 // create react components area for event handling
 const App = () => {
-	return <div>Hi there!</div>;
+	return (
+		<div className="ui container comments">
+			<div className="comment">
+				<a href="/" className="avatar">
+					<img alt="avatar" />
+				</a>
+				<div className="content">
+					<a href="/" className="author">
+						Sam
+					</a>
+					<div className="metadata">
+						<span className="date">Today at 6:00PM</span>
+					</div>
+					<div className="text">Nice blog post!</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 // Displaying the react on the screen
