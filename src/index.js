de8eb3df5515b7f23aa4b44839ddf1d99ae6e6cc
Clucas0311 <clucas0311@gmail.com>
2020-11-13 22:34:21 -0500

    Creating props for time

--- src/index.js
+++ src/index.js
@@ -9,7 +9,9 @@ const App = () => {
 	return (
 		<div className="ui container comments">
 			{/* // show a component inside another  */}
-			<CommentDetail />
+			<CommentDetail author="Sam" timeAgo="Today at 4:45PM" />
+			<CommentDetail author="Alex" timeAgo="Today at 2:00AM" />
+			<CommentDetail author="Jane" timeAgo="Yesterday at 5:00PM" />
 		</div>
 	);
 };
