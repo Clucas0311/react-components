6d484cff6c58354c38ef369f4cc3efdf536e9269
Clucas0311 <clucas0311@gmail.com>
2020-11-13 23:08:01 -0500

    Creating Props Children

--- src/index.js
+++ src/index.js
@@ -3,30 +3,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 // create react components area for event handling
 const App = () => {
 	return (
 		<div className="ui container comments">
 			{/* // show a component inside another  */}
-			<CommentDetail
-				author="Sam"
-				timeAgo="Today at 4:45PM"
-				content="Nice Blog Post!"
-				avatar={faker.image.avatar()}
-			/>
-			<CommentDetail
-				author="Alex"
-				timeAgo="Today at 2:00AM"
-				content="I like the subject!"
-				avatar={faker.image.avatar()}
-			/>
-			<CommentDetail
-				author="Jane"
-				timeAgo="Yesterday at 5:00PM"
-				content="I like the writing!"
-				avatar={faker.image.avatar()}
-			/>
+			<ApprovalCard>
+				<CommentDetail
+					author="Sam"
+					timeAgo="Today at 4:45PM"
+					content="Nice Blog Post!"
+					avatar={faker.image.avatar()}
+				/>
+			</ApprovalCard>
+			<ApprovalCard>
+				<CommentDetail
+					author="Alex"
+					timeAgo="Today at 2:00AM"
+					content="I like the subject!"
+					avatar={faker.image.avatar()}
+				/>
+			</ApprovalCard>
+			<ApprovalCard>
+				<CommentDetail
+					author="Jane"
+					timeAgo="Yesterday at 5:00PM"
+					content="I like the writing!"
+					avatar={faker.image.avatar()}
+				/>
+			</ApprovalCard>
 		</div>
 	);
 };
